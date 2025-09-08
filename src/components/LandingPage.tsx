81c73efb6963f328752c8debb89e31d2a4597905
rajalakshmi-growphil <rajalakshmifullstack@gmail.com>
2025-09-08 17:24:06 +0530

    Update LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { 
   Shield, 
   Bot, 
   Users, 
   FileText, 
-  Clock, 
   CheckCircle,
   ArrowRight,
   Stethoscope,
